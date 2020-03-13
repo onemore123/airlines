7c564fca57869ef1f99a18fc11af900573061d07
Onemore <onemore04@mail.ru>
2020-03-13 22:10:09 +0700

    SIT Get flight available seats by type added
    Signed-off-by: Onemore <onemore04@mail.ru>

--- src/main/java/com/example/airlines/controller/FlightController.java
+++ src/main/java/com/example/airlines/controller/FlightController.java
@@ -1,8 +1,12 @@
 package com.example.airlines.controller;
 
+import com.example.airlines.entity.Flight;
+import com.example.airlines.enums.SeatType;
+import com.example.airlines.exception.NotFoundException;
 import com.example.airlines.repository.FlightRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +21,18 @@ public class FlightController {
     @GetMapping
     public String list() {
         return flightRepository.findAll().toString();
+    }
+
+    @GetMapping("/info")
+    public int getAvailableSeats(@RequestParam Long flightId, @RequestParam String type) {
+        Flight flight = flightRepository.findById(flightId).orElseThrow(NotFoundException::new);
+
+        if (SeatType.BUSINESS_CLASS.name().equals(type)) {
+            return flight.getBusinessSeatsAvailableNumber();
+        } else if (SeatType.ECONOMY_CLASS.name().equals(type)) {
+            return flight.getEconomySeatsAvailableNumber();
+        }
+
+        return -1;
     }
 }
